5d02b5bc47587497f8e67ba88c88a96475109296
raredead <smahelzed@gmail.com>
2023-09-26 20:41:04 +0700

    Файлы Footer

---
+++ src/components/Layout/Footer/FooterProject.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="footer">
+        <span class="footer__txt">all rights reserved</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped src="./FooterProject.scss"/>
