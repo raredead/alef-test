40ff8be0d79c3abb246b634b89e875151a72aa35
raredead <smahelzed@gmail.com>
2023-09-26 20:38:13 +0700

    Папка страниц проекта

---
+++ src/Views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <header-project />
+      <form-project />
+      <footer-project />
+    </div>
+  </template>
+  
+  <script setup>
+  import HeaderProject from '@/components/Layout/Header/HeaderProject.vue'
+  import FormProject from '@/components/FormProject/FormProject.vue';
+  import FooterProject from '../components/Layout/Footer/FooterProject.vue';
+  </script>
