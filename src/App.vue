49c90037ab9768c63cfab8729c70170bdac9aede
raredead <smahelzed@gmail.com>
2023-09-26 15:44:24 +0700

    Сверстал шапку

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header-project />
+  </div>
+</template>
+
+<script setup>
+import HeaderProject from './components/Layout/Header/HeaderProject.vue'
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,500&family=Montserrat:wght@400;500;700&family=Open+Sans:wght@300&family=Roboto+Slab:wght@500&family=Rubik:wght@300;500&display=swap');
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/Layout/Header/HeaderProject.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="header">
+        <div class="container">
+            <div class="header-main">
+                <div class="header-main__logo">
+                    <img src="../../../assets/header/logo.svg" alt="" class="header-logo">
+                </div>
+                <div class="header-main__btns btns-header-main">
+                    <a class="btns-header-main__btn" :href="item.path" v-for="(item, i) of menu" :key="i">{{ item.name }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const menu = [
+    {
+        name: 'Форма',
+        path: '/forms'
+    },
+    {
+        name: 'Превью',
+        path: '/preview'
+    }
+]
+</script>
+
+<style lang="scss" scoped src="./HeaderProject.scss" />
